d2819e08147c2cea5f7a534b84d2f446fc6bfe45
Courtney Haltiwanger <chalt0319@gmail.com>
2018-06-28 18:57:52 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -3,10 +3,17 @@ class Patient
   
   def initialize(name)
     @name = name 
+    @appointments = [] 
   end 
   
   def new_appointment(doctor, date)
-    Appointment.new(self, doctor, date)  
+    new_app = Appointment.new(self, doctor, date)  
+    @appointments << new_app 
+    new_app 
+  end 
+  
+  def appointments 
+    @appointments
   end 
   
 end 
