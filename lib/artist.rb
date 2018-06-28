d6618ec963d8e070317cc41f87d7757f9d9dfb50
Courtney Haltiwanger <chalt0319@gmail.com>
2018-06-28 17:09:53 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,24 @@
+require 'pry'
+
+class Artist 
+  attr_accessor :name 
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << self
+    @songs = []
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def new_song(name, genre)
+     @songs << Song.new(name, self, genre)
+  end 
+  
+  def songs
+    @songs 
+  end 
+end 
