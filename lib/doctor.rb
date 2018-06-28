c5d414467334a3d2f9cf6407ad49e82179e157ab
Courtney Haltiwanger <chalt0319@gmail.com>
2018-06-28 18:54:51 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -14,7 +14,7 @@ class Doctor
     @@all
   end 
   
-  def new_appointment(date, patient)
+  def new_appointment(patient, date)
     new_app = Appointment.new(patient, self, date)
     @appointments << new_app
     new_app
